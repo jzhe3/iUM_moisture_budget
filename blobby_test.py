b05f2b1bf0bc42e22e69e14b2ef084ae369eb1c2
Mark Muetzelfeldt <markmuetz@gmail.com>
2016-10-27 13:58:16 +0100

    Fix blobby test to properly get MC on/off expts.

--- blobby_test.py
+++ blobby_test.py
@@ -29,42 +29,51 @@ def load_data():
     proc.load_modules()
     atmos4ncs = dag.get_nodes('atmos.4.nc')
 
-    Mqrains = []
-    atmos4s = []
+    Mqrains = {}
+    atmos4s = {}
     for atmos4nc in atmos4ncs:
+        print(atmos4nc.group)
         cubes = pe.load(atmos4nc)
-        atmos4s.append(cubes)
+        atmos4s[atmos4nc.group.name[4:]] = cubes
         stash.rename_unknown_cubes(cubes, True)
         rho_R2 = cubes.get_cube(0, 253)
         rain = cubes.get_cube(0, 272)
         proc.data = [(rho_R2, rain)]
         Mqrain = proc.run()
-        Mqrains.append(Mqrain)
+        Mqrains[atmos4nc.group.name[4:]] = Mqrain
 
     atmos5ncs = dag.get_nodes('atmos.5.nc')
-    atmos5s = []
+    atmos5s = {}
     for atmos5nc in atmos5ncs:
+        print(atmos5nc.group.name[4:])
         cubes = pe.load(atmos5nc)
-        atmos5s.append(cubes)
+        atmos5s[atmos5nc.group.name[4:]] = cubes
         stash.rename_unknown_cubes(cubes, True)
 
     atmos6ncs = dag.get_nodes('atmos.6.nc')
-    atmos6s = []
+    atmos6s = {}
     for atmos6nc in atmos6ncs:
+        print(atmos6nc.group.name)
         cubes = pe.load(atmos6nc)
-        atmos6s.append(cubes)
+        atmos6s[atmos6nc.group.name[4:]] = cubes
         stash.rename_unknown_cubes(cubes, True)
 
     return Mqrains, atmos4s, atmos5s, atmos6s
 
 
-def tracked_profiles(Mqrains, atmos4s, atmos5s, atmos6s, ts0, ba0, ts1, ba1):
+def tracked_profiles(Mqrains, atmos4s, atmos5s, atmos6s, tss, bas):
     all_qvars = []
     itrs = []
-    for Mqrain, atmos4, atmos5, atmos6, ts, ba, start_coord in zip(Mqrains, atmos4s, atmos5s, atmos6s,
-                                                                   [ts0, ts1], 
-                                                                   [ba0, ba1],
-                                                                   [(0, 6), (0, 2)]):
+    start_coords = {'MC_on': (0, 2), 'MC_off': (0, 6)}
+    for expt in ['MC_on', 'MC_off']:
+        Mqrain = Mqrains[expt]
+        atmos4 = atmos4s[expt]
+        atmos5 = atmos5s[expt]
+        atmos6 = atmos6s[expt]
+        ts = tss[expt]
+        ba = bas[expt]
+        start_coord = start_coords[expt]
+
         sb = ts[start_coord[0]][start_coord[1]]
         print(sb)
         q = atmos4.get_cube(0, 10)
@@ -112,6 +121,7 @@ def tracked_profiles(Mqrains, atmos4s, atmos5s, atmos6s, ts0, ba0, ts1, ba1):
                 continue
             b, cmax = itr.next()
             print(title)
+            print(cmax)
             plt.figure(title)
             plt.clf()
             for qvarname, qvar in qvars:
@@ -139,10 +149,12 @@ def tracked_profiles(Mqrains, atmos4s, atmos5s, atmos6s, ts0, ba0, ts1, ba1):
 if __name__ == '__main__':
     thresh = float(sys.argv[1])
     Mqrains, atmos4s, atmos5s, atmos6s = load_data()
-    ts0, ba0 = om.experimental.blobby.create_blob_timeseries(Mqrains[0], thresh)
+    tss = {}
+    bas = {}
+    tss['MC_on'], bas['MC_on'] = om.experimental.blobby.create_blob_timeseries(Mqrains['MC_on'], thresh)
     # om.experimental.blobby.render_graph(ts0, 'MC_on.png')
-    ts1, ba1 = om.experimental.blobby.create_blob_timeseries(Mqrains[1], thresh)
+    tss['MC_off'], bas['MC_off'] = om.experimental.blobby.create_blob_timeseries(Mqrains['MC_off'], thresh)
     # om.experimental.blobby.render_graph(ts1, 'MC_off.png')
     # om.experimental.blobby.plot(Mqrains, thresh)
-    tracked_profiles(Mqrains, atmos4s, atmos5s, atmos6s, ts0, ba0, ts1, ba1)
+    tracked_profiles(Mqrains, atmos4s, atmos5s, atmos6s, tss, bas)
 
